a46b6d1aa4f3d54c63a311867bd79623b74a2708
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-04-05 19:41:13 +0100

    refactor: remove Azure Application Insights role assignment configuration

--- terraform/app_insights_role.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "web_app_insights_reader" {
-  scope                = data.azurerm_application_insights.app_insights.id
-  role_definition_name = "Monitoring Reader"
-  principal_id         = local.web_identity.principal_id
-}
