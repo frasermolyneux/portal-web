d520061fa732b4e54b96a6580736fead137fc508
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-04-13 20:09:50 +0100

    feat: Remove search filters from servers index and simplify DataTable configuration

--- src/XtremeIdiots.Portal.Web/wwwroot/js/servers-index.js
+++ src/XtremeIdiots.Portal.Web/wwwroot/js/servers-index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     const serversTable = $('#serversTable');
     
     if (serversTable.length && serversTable.find('tbody tr').length > 0) {
-        const dataTable = serversTable.DataTable({
+        serversTable.DataTable({
             responsive: {
                 details: {
                     type: 'inline',
@@ -11,37 +11,24 @@ $(document).ready(function () {
                 }
             },
             autoWidth: false,
-            paging: true,
-            pageLength: 25,
+            paging: false,
+            searching: false,
+            info: false,
             order: [], // Preserve server-side ordering (ServerListPosition)
             columnDefs: [
-                { targets: 0, responsivePriority: 6, orderable: false, width: '30px' }, // Game icon - not sortable
-                { targets: 1, responsivePriority: 1 }, // Title - always visible, sortable
-                { targets: 2, responsivePriority: 3 }, // Hostname - medium priority, sortable
-                { targets: 3, responsivePriority: 5, orderable: false }, // Links - not sortable
-                { targets: 4, responsivePriority: 4, orderable: false }, // Players - not sortable
-                { targets: 5, responsivePriority: 7 }, // Map - sortable
-                { targets: 6, responsivePriority: 8 }, // Mod - sortable
-                { targets: 7, responsivePriority: 2, orderable: false }  // Actions - not sortable
+                { targets: 0, responsivePriority: 6, orderable: false, width: '30px' }, // Game icon
+                { targets: 1, responsivePriority: 1 }, // Title
+                { targets: 2, responsivePriority: 3 }, // Hostname
+                { targets: 3, responsivePriority: 5, orderable: false }, // Links
+                { targets: 4, responsivePriority: 4, orderable: false }, // Players
+                { targets: 5, responsivePriority: 7 }, // Map
+                { targets: 6, responsivePriority: 8 }, // Mod
+                { targets: 7, responsivePriority: 2, orderable: false }  // Actions
             ],
             language: {
-                lengthMenu: 'Show _MENU_ entries',
-                info: 'Showing _START_ to _END_ of _TOTAL_ servers',
                 emptyTable: 'No servers found'
             },
-            dom: 'lfrtip' // Default layout
+            dom: 'rt' // Table only, no search/paging/info controls
         });
-
-        // Connect custom search input to DataTable
-        const searchInput = $('#serversSearchInput');
-        if (searchInput.length) {
-            // Bind the custom search input to DataTable search
-            searchInput.on('keyup search input', function () {
-                dataTable.search(this.value).draw();
-            });
-        }
-
-        // Hide the default DataTables search box
-        $('.dataTables_filter').hide();
     }
 });
